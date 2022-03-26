b9f0bf88484896cc280fc44d3ea7b1f1dead981e
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 15:46:36 +0300

    sum

---
+++ proje73.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+	
+	//**************************************************************************
+	
+	//int tekMiCiftMi(int a)
+	//{
+		//if(a%2==0)
+		//{
+			//printf("Bu Sayi Cifftir.",a);
+		//}
+		//else
+		//{
+			//printf("Bu Sayi Tektir.",a);
+		//}
+	//}
+	
+	//int main(){
+		
+		//int sayi;
+		//printf("Lutfen Bir sayi Giriniz: ");
+		//scanf("%d",&sayi);
+		//tekMiCiftMi(sayi);
+	//}
+	
+	
+	//**************************************************************************
+	
+	//int toplamaIslemi(int a, int b)
+    //{
+    //int topla=a+b;
+    //printf("\nToplama Islemi:%d",topla);
+    //}
+    
+	//int cikarmaIslemi(int a, int b)
+    //{
+    //int cikarma;
+    
+       //if(a>b)
+       //{
+       	//cikarma=a-b;
+	   //}
+	   //else if(b>a)
+	   //{
+	   	//cikarma=b-a;
+	   //}
+    //printf("\nCikarma Islemi:%d",cikarma);
+    //}
+    
+    //int main() {
+	
+	//int s1;
+	//int s2;
+	
+	//printf("Birinci Sayiyi Giriniz: ");
+	//scanf("%d",&s1);
+	//printf("Ikinci Sayiyi Giriniz: ");
+	//scanf("%d",&s2);
+	
+	//toplamaIslemi(s1,s2);
+	//cikarmaIslemi(s1,s2);
+	
+	
+	//**************************************************************************
+	
+	//void sayiSiralama(int a, int b)
+    //{
+    	//if(a<b)
+    	//{
+    		//printf("%d<%d",a,b);
+		//}
+		//else if(b<a)
+		//{
+			//printf("%d<%d",b,a);
+		//}
+		//else
+		//{
+			//printf("%d=%d",a,b);
+		//}
+	//}
+
+//int main() {
+	
+	//int s1;
+	//int s2;
+	
+	//printf("Birinci Sayiyi Giriniz: ");
+	//scanf("%d",&s1);
+	//printf("Ikinci Sayiyi Giriniz: ");
+	//scanf("%d",&s2);
+	
+	//sayiSiralama(s1,s2);
+	
+	
+	//**************************************************************************
+	
+	
+	//int i,j;
+	//int matris[3][3];
+	
+	//for(i=0;i<3;i++)
+	//{
+		//for(j=0;j<3;j++)
+		//{
+			//printf("Su an %d sutununda bulunuyorsunuz.\nLutfen %d satirini doldurunuz: ",i,j);
+			//scanf("%d",&matris[i][j]);
+		//}
+		//printf("\n");
+	//}
+	
+	//for(i=0;i<3;i++)
+	//{
+		//for(j=0;j<3;j++)
+		//{
+			//printf("%d",matris[i][j]); 
+		//}
+		//printf("\n");
+    //}
+    
+    
+    
+//}
